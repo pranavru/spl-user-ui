18801dc853c1223c83623a5d8b3d50d1e9428510
Pranav Ruparelia <pranavruparelia9@gmail.com>
2024-10-18 13:34:59 -0400

    feat: added user references

--- src/components/pages/users/columns.tsx
+++ src/components/pages/users/columns.tsx
@@ -5,6 +5,7 @@ import { ContactInfo } from "../common/columns/contact-info";
 import { Roles } from "../common/columns/roles";
 import { MandalColumn } from "../common/columns/mandal";
 import { ColumnTypography } from "../common/columns/column-typography";
+import { ReferenceContactsColumn } from "./views/columns/reference-contacts";
 
 export const columns: GridColDef[] = [
   { field: 'name', headerName: 'Name', width: 150, renderCell: (params) => <ColumnTypography params={params} />},
@@ -13,5 +14,6 @@ export const columns: GridColDef[] = [
   { field: 'dateOfBirth', headerName: 'DOB', width: 150, renderCell: (params) => <ColumnTypography params={params} />},
   { field: 'mandal', headerName: 'Mandal', width: 250, renderCell: MandalColumn },
   { field: 'role', headerName: 'Role', width: 150, renderCell: Roles},
+  { field: 'referenceContacts', headerName: 'Reference Contacts', width: 250,  renderCell: (params) => <ReferenceContactsColumn params={params} />},
   { field: 'isActive', headerName: 'Active', width: 100, renderCell: ActiveToggle },
 ];
